045a2c93994d3b89ca324371a15bc32c6a1a80a3
h-e-l-i-o <helio@ljeto.com>
2021-09-09 19:34:58 +0200

    browse, search, delete, create

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 
 const indexRouter = require('./routes/index')
-const usersRouter = require('./routes/users')
 
 const app = express()
 
@@ -20,15 +19,15 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
+app.use('/delete/:oid', indexRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404))
 })
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,124 @@
 const express = require('express')
 const router = express.Router()
+const fs = require('fs')
+const Fuse = require('fuse.js')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' })
+const dataPath = './db/VehicleInfo.json' // path to our JSON file
+
+const mongoObjectId = function () {
+  const timestamp = (new Date().getTime() / 1000 | 0).toString(16)
+  return timestamp + 'xxxxxxxxxxxxxxxx'.replace(/[x]/g, function () {
+    return (Math.random() * 16 | 0).toString(16)
+  }).toLowerCase()
+}
+
+const getVehicleData = () => {
+  const jsonData = fs.readFileSync(dataPath)
+  return JSON.parse(jsonData)
+}
+const saveVehicleData = (data) => {
+  const stringifyData = JSON.stringify(data)
+  fs.writeFileSync(dataPath, stringifyData)
+}
+const getVehiclesPage = (filteredVehicles, page = 1, limit = 50) => {
+  const startIndex = (page - 1) * limit
+  const endIndex = page * limit
+  const result = filteredVehicles.slice(startIndex, endIndex)
+  return result
+}
+const fuzzySearch = (list, pattern) => {
+  const options = {
+    // isCaseSensitive: false,
+    // includeScore: false,
+    // shouldSort: true,
+    // includeMatches: false,
+    // findAllMatches: false,
+    // minMatchCharLength: 1,
+    // location: 0,
+    threshold: 0.6,
+    // distance: 100,
+    // useExtendedSearch: false,
+    // ignoreLocation: false,
+    // ignoreFieldNorm: false,
+    keys: [
+      'title'
+    ]
+  }
+  const fuse = new Fuse(list, options)
+  return fuse.search(pattern)
+}
+
+/* browse vehicles. */
+router.get('/', function (req, res, next) {
+  const limit = 50
+  const page = {}
+  page.current = req.query.page ? Number(req.query.page) : 1
+  page.next = page.current + 1
+  page.previous = page.current - 1
+  const filter = req.query.filter
+
+  const allVehicles = getVehicleData()
+  const extendedVehicles = allVehicles.map(vehicle => {
+    vehicle.title = vehicle.make + vehicle.model + vehicle.year
+    return vehicle
+  })
+
+  let filteredVehicles = extendedVehicles
+  let filtered = false
+  if (filter) {
+    filteredVehicles = fuzzySearch(allVehicles, filter)
+    filtered = true
+  }
+  const count = filteredVehicles.length
+
+  const forward = (page.current * limit) < count
+  const back = page.current > 1
+  const vehicles = getVehiclesPage(filteredVehicles, page.current)
+  res.render('index', { count, vehicles, page, filter, filtered, forward, back })
+})
+
+// delete - using post method because delete not allowed from html
+router.get('/delete/:oid', (req, res) => {
+  const allVehicles = getVehicleData()
+  const oid = req.params.oid
+  const vehicleIndex = allVehicles.findIndex(vehicle => vehicle._id.$oid === oid)
+  const vehicleFound = vehicleIndex > -1
+  if (vehicleFound) {
+    allVehicles.splice(vehicleIndex, 1)
+    saveVehicleData(allVehicles)
+  }
+  res.render('deleted', { vehicleFound })
+})
+
+// delete - using post method because delete not allowed from html
+router.post('/', (req, res) => {
+  let message = ''
+  if (isNaN(req.body.year)) return res.render('created', { message: 'Year must be a number', vehicleExists: false })
+  const allVehicles = getVehicleData()
+  const checkDuplicate = () => {
+    const vehicleIndex = allVehicles.findIndex(vehicle => {
+      return (
+        vehicle.make.toUpperCase() === req.body.make.toUpperCase() &&
+        vehicle.model.toUpperCase() === req.body.model.toUpperCase() &&
+        vehicle.year === Number(req.body.year)
+      )
+    })
+    return vehicleIndex > -1
+  }
+  const vehicleExists = checkDuplicate()
+  if (vehicleExists) {
+    message = 'Vehicle already exist'
+  } else {
+    allVehicles.push({
+      _id: { $oid: mongoObjectId() },
+      make: req.body.make.toUpperCase(),
+      model: req.body.model.toUpperCase(),
+      year: Number(req.body.year)
+    })
+    saveVehicleData(allVehicles)
+    message = 'Vehicle added succesfully'
+  }
+  res.render('created', { vehicleExists, message })
 })
 
 module.exports = router
